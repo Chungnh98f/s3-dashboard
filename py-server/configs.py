ab02a426293eae8fc85c9d8c643e96230366e291
hungpq96 <quanghwng22@gmail.com>
2021-12-05 18:19:10 +0700

    Containerize py-server

--- py-server/configs.py
+++ py-server/configs.py
@@ -1,12 +1,22 @@
-# TODO: move values to .env
+from os import environ
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 class AppConfig:
-    DEBUG = True
-    TEST = True
-    ENV = 'development'
+    class Dev:
+        DEBUG = True
+        TEST = True
+        ENV = 'development'
+
+    class Prod:
+        DEBUG = False
+        Test = False
+        ENV = 'production'
 
 
 class S3Config:
-    SECRET_KEY = 'foobar'
-    ACCESS_KEY = 'foobar'
-    URL = 'http://localhost:4566/'
+    SECRET_KEY = environ.get('AWS_SECRET_KEY')
+    ACCESS_KEY = environ.get('AWS_ACCESS_KEY')
+    URL = environ.get('AWS_ENDPOINT')
--- py-server/wsgi.py
+++ py-server/wsgi.py
@@ -1,7 +1,16 @@
 from app import create_app
 from configs import AppConfig
+from dotenv import load_dotenv
+from os import environ
+
+
+load_dotenv()
+
+app = create_app(
+    AppConfig.Dev if environ.get('APP_ENV') == 'development'
+    else AppConfig.Prod
+)
 
 
 if __name__ == '__main__':
-    app = create_app(AppConfig)
     app.run(host='0.0.0.0')
